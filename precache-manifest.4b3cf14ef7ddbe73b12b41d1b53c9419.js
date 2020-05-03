06e092c7b918f4f2ad26a054e949d9d524787ffa
quipcode <abdulkarim8ahmed@gmail.com>
2020-05-02 22:27:59 -0700

    Updates

---
+++ precache-manifest.4b3cf14ef7ddbe73b12b41d1b53c9419.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c111d4766088a1c90f762dfbdaaf953b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d5912b8f8cc19b17234",
+    "url": "/static/css/2.78203440.chunk.css"
+  },
+  {
+    "revision": "efcf5d399892a2b5d249",
+    "url": "/static/css/main.86081bdb.chunk.css"
+  },
+  {
+    "revision": "0d5912b8f8cc19b17234",
+    "url": "/static/js/2.a8e909c4.chunk.js"
+  },
+  {
+    "revision": "729937898630e839994162d5d9f8ce89",
+    "url": "/static/js/2.a8e909c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcf5d399892a2b5d249",
+    "url": "/static/js/main.41c7f088.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "bc21be92653b407bc1dff348acf541ed",
+    "url": "/static/media/Clipart-Email-9158026.bc21be92.png"
+  },
+  {
+    "revision": "e66e26a6e3c218f7748dd0bd9cb034ff",
+    "url": "/static/media/MISTRAL.e66e26a6.TTF"
+  },
+  {
+    "revision": "075875aae52ad9b1ba9018ba7f4e66a0",
+    "url": "/static/media/Mistral.075875aa.woff"
+  },
+  {
+    "revision": "77469547f0dc525989b94680e2d913e9",
+    "url": "/static/media/Mistral.77469547.eot"
+  },
+  {
+    "revision": "cf9898b98dd2e1c3abede5996f207ca6",
+    "url": "/static/media/Mistral.cf9898b9.woff2"
+  },
+  {
+    "revision": "ed46437fc15f71c579cf1d60f05f37f2",
+    "url": "/static/media/Mistral.ed46437f.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "3debcaf1a98e4445d213fd0089d42f6b",
+    "url": "/static/media/fa-brands-400.3debcaf1.svg"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89f9806b964f92ad282fd60947eba588",
+    "url": "/static/media/fa-regular-400.89f9806b.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "ad912fd102f4052d3273dd838e0c671d",
+    "url": "/static/media/fa-solid-900.ad912fd1.svg"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "23af55bb9d1868a8e7ef810202be9cdf",
+    "url": "/static/media/profilePhoto.23af55bb.jpg"
+  }
+]);
